ff9af6e1bd7fa390beee38391e12de128ca16553
MIBCS <aryanjadhavart2005@gmail.com>
2025-03-06 19:57:02 +0530

    Initial commit

---
+++ poly.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+const int maxsize = 100;
+
+typedef struct{
+    float coef;
+    int exp;
+}poly_term;
+
+class polynom{
+    private :
+        poly_term poly[maxsize];
+        int total_terms;
+    public : 
+        polynom(){
+            total_terms = 0;
+        }
+        
+        void read_poly(){
+            cout << "Enter the no. of total terms : ";
+            cin  >> total_terms;
+            
+            for(int i=0; i<total_terms; i++){
+                cout << "Enter the coefficient " << i+1 << " : ";
+                cin >> poly[i].coef;
+                cout << "Enter the exponent " << i+1 << " : ";
+                cin >> poly[i].exp;
+            }
+        }
+        
+        void display_poly() {
+            for (int i = 0; i < total_terms; i++) {
+                cout << poly[i].coef << "x^" << poly[i].exp;
+                if (i != total_terms - 1) {
+                    cout << " + ";
+                }
+            }
+            cout << endl; 
+        }
+
+        void evalulate_poly(double value){
+            double result = 0;
+            for(int i=0; i<total_terms; i++){
+                result += poly[i].coef * pow(value, poly[i].exp);
+            }
+            cout << "The result of the polynomial is : " << result << endl;
+        }
+
+        void add_poly(polynom b){
+            polynom c;
+            int i = 0, j = 0;
+
+            while (i < total_terms && j < b.total_terms){
+                if(poly[i].exp < b.poly[j].exp){
+                    c.poly[c.total_terms++] = b.poly[j++];
+                }
+
+                else if(poly[i].exp > b.poly[j].exp){
+                    c.poly[c.total_terms++] = poly[i++];
+                }
+
+                else {
+                    c.poly[c.total_terms].exp = poly[i].exp;
+                    c.poly[c.total_terms++].coef = poly[i++].coef + b.poly[j++].coef;
+                }
+            }
+
+            while (i < total_terms){
+                c.poly[c.total_terms++] = poly[i++];
+            }
+
+            while(j < b.total_terms){
+                c.poly[c.total_terms++] = b.poly[j++];
+            }
+
+            c.display_poly();
+        }
+};
+        
+
+int main(){
+    polynom a,b;
+    a.read_poly();
+    b.read_poly();
+    cout << "The first polynomial is : ";
+    a.display_poly();
+    cout << "The second polynomial is : ";
+    b.display_poly();
+    cout << "The addition of the two polynomials is : ";
+    a.add_poly(b);
+    a.evalulate_poly(1);
+    return 0;
+}
---
+++ quickSort.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int quickSort(int arr[], int low, int high){
+    int pivot = arr[low];
+    int left = low + 1; right = high;
+    
+    for(int i = low + 1; i <= high; i++){
+        if(arr[i] pivot)
+    }
+}
+
+int main(){
+
+int n;
+cout << "Enter the no. of elements : ";
+cin >> n;
+int arr[n];
+
+cout << "Enter the elements : ";
+for(int i = 0; i < n; i++){
+    cin >> arr[i];
+}
+
+cout << "[";
+for(int i = 0; i < n; i++){
+    cout << " " << arr[i] << " ";
+}cout << "]";
+
+int pivot = arr[0];
+int a = 0, b = 0;
+
+for(int i = )
+
+    return 0;
+}
